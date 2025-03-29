a31dea39c0feafb8e5bfc702849b196f5265bc08
Ariet2003 <amanbekovariet405@gmail.com>
2025-03-29 16:26:20 +0600

    fix

--- prisma/migrations/20240329_update_settings_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Settings" (
-    "id" TEXT NOT NULL,
-    "telegramId" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Settings_pkey" PRIMARY KEY ("id")
-); 
