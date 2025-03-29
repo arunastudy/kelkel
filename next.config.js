161c90bc7def0e0152919f6877fef15b29d12dd7
Ariet2003 <amanbekovariet405@gmail.com>
2025-03-29 07:32:07 +0600

    deploy to versel

--- next.config.js
+++ next.config.js
@@ -3,11 +3,13 @@ const nextConfig = {
   output: 'standalone',
   images: {
     domains: ['62.113.41.23'],
-    unoptimized: true,
+    minimumCacheTTL: 60,
   },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  swcMinify: true,
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig; 
